afcfc9688f55c398ddd40a2d53e825d0e598a4aa
RMS <RMS@FS-G13.velan2.local>
2022-02-10 05:34:28 +0530

    File01 Commit02 AddProgram

--- File01.py
+++ File01.py
@@ -1,2 +1,5 @@
-a=int(input("Eneter your name: "))
-print(a)
+#Addition Program
+
+number01=int(input("Enter Number01: "))
+number02=int(input("Enter Number02: "))
+print("Total Value of the given number is: ", number01+number02)
